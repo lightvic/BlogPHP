34236040a2652d00c8a2356be92f6a3e2e4a5e86
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-11-22 19:01:05 +0100

    juste un peu de style, très moche, pour séparer les articles

--- app/views/home.php
+++ app/views/home.php
@@ -24,11 +24,14 @@ var_dump($_SESSION);
 <?php
 /** @var App\Entity\Post[] $posts */
 foreach ($posts as $post) {
-	echo("Id du post : " . $post->getId() . " - date : " . $post->getDate() . " - id de l'auteur : " . $post->getUser());
-	echo('</br>');
-	echo($post->getContent());
-	echo('</br>');
-	echo('</br>');
+	echo "<div id='post-" . strval($post->getId()) . "' style='border: 1px solid'>";
+	echo("Id du post : " . $post->getId() . " - id de l'auteur : " . $post->getUser()); // ligne à retirer pour la suite : ne sert qu'au debugging
+	echo "<h4>Auteur du post</h4>"; // Je dois créer une fonction qui me permet de récupérer le NOM de l'auteur d'un post
+	echo "<div>" . $post->getContent() . "</div>";
+	echo "<div>" . $post->getDate() . "</div>";
+	echo "</div>";
+	echo "</br>";
+	echo "</br>";
 }
 
 
